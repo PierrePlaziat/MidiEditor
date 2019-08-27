513883e6456a954b6a253182076ef19df4fa997c
Nihilhis <pierre.plaziat@live.fr>
2019-08-27 21:56:51 +0200

    RefactorProject

---
+++ VsProject/ScoreApp/MVC/Model.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Forms;
+using Sanford.Multimedia.Midi;
+using Sanford.Multimedia.Midi.UI;
+
+namespace ScoreApp.MVC
+{
+    class Model
+    {
+
+        public List<Track> Tracks;
+        //{
+        //    get
+        //    {
+        //        return tracks;
+        //    }
+        //    set
+        //    {
+        //        tracks = value; RaisePropertyChanged("Tracks");
+        //    }
+        //}
+        //private List<Track> tracks;
+
+
+        public string ProjectName = "New Project";
+        public Sequencer sequencer = new Sequencer();
+        public Sequence sequence = new Sequence();
+
+        public OutputDevice outDevice;
+        public int outDeviceID = 0;
+        public OutputDeviceDialog outDialog = new OutputDeviceDialog();
+        
+        public Timer timer = new Timer();
+
+        public bool scrolling = false;
+        public bool playing = false;
+        public bool closing = false;
+
+        public OpenFileDialog openMidiFileDialog = new OpenFileDialog();
+
+    }
+
+}
---
+++ VsProject/ScoreApp/TrackLine/MvcMidi/Midi_View.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Sanford.Multimedia.Midi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScoreApp.TrackLine.MvcMidi
+{
+
+    public partial class Midi_View : Page
+    {
+
+        Midi_Control ctrl;
+        Midi_Model model;
+
+        public Midi_View(Track track,Sequencer sequencer)
+        {
+            model = new Midi_Model(track,sequencer);
+            InitializeComponent();
+            ctrl = new Midi_Control(model,this);
+        }
+    }
+
+}
